6fb02108404786afa7be7789a2f61c43baf39c99
Silas Pereira <silasapb@al.insper.edu.br>
2026-05-06 13:20:57 -0300

    inicio da logica

---
+++ src/Main/Main.fs
@@ -0,0 +1,23 @@
+﻿module Main.Main
+
+open IO.DataLoader
+open IO.RawLoader
+open PureDomain.Types
+
+let run () = async {
+    // Baixa dados se necessário
+    if not (System.IO.File.Exists(System.IO.Path.Combine(__SOURCE_DIRECTORY__, "..", "..", "dados", "dados_consolidados.csv"))) then
+        do! downloadDados()
+    
+    // Carrega dados consolidados
+    let priceData = loadConsolidatedData()                                          
+    printfn "Dados carregados: %d tickers, %d dias" priceData.Tickers.Length priceData.Prices.Length
+    
+    // Resto da lógica aqui
+    printfn "Iniciando simulação..."
+}
+
+[<EntryPoint>]
+let main argv =
+    run() |> Async.RunSynchronously
+    0

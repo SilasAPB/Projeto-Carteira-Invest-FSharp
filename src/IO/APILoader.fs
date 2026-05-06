449ca3db89420419e6ceccfbbcdb3dda9f319855
Silas Pereira <silasapb@al.insper.edu.br>
2026-05-06 13:17:35 -0300

    codigos de extracao

---
+++ src/IO/APILoader.fs
@@ -0,0 +1,12 @@
+namespace IO
+open System
+open System.Net.Http
+open System.Threading.Tasks
+
+module APILoader =
+    let getDados () : Async<unit> =
+        async {
+            // Aqui você implementaria a lógica para baixar os dados de uma API
+            // Exemplo: usar HttpClient para fazer uma requisição GET e salvar o resultado em um arquivo CSV
+            
+        }

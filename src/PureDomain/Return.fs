6495acab3d64e4ec6eeec9faa91438d3016326f7
Silas Pereira <silasapb@al.insper.edu.br>
2026-05-08 10:48:11 -0300

    mudança na estrutura do projeto -simplificacao

--- src/PureDomain/Return.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace PureDomain
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- src/PureDomain/Types.fs
+++
@@ -1,7 +0,0 @@
-// Define tipos de dados para a aplicação
-module PureDomain.Types
-
-type PriceData = {
-    Tickers: string[]
-    Prices: decimal[][]  // [dia][ação] - matriz de preços
-}
